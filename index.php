0d243548ac53ccc72be936230a3487781119605f
caioestevao1000 <caioestevao_@hotmail.com>
2024-06-09 22:46:39 -0300

    Criação dos arquivos PHP e construção da estrutura do html

--- index.php
+++ index.php
@@ -154,15 +154,15 @@
       <h3 class="float-md-start mb-0">Cover</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Início</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Lançamentos</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Contato</a>
+        <a class="nav-link fw-bold py-1 px-0" href="cadastro.php">Cadastro</a>
+        <a class="nav-link fw-bold py-1 px-0" href="login.php">Login</a>
       </nav>
     </div>
   </header>
 
   <main class="px-3">
     <h1>Página de Início.</h1>
-    <p class="lead">A capa é um modelo de uma página para construir páginas iniciais simples e bonitas. Baixe, edite o texto e adicione sua própria foto de fundo em tela cheia para torná-la sua.</p>
+    <p class="lead">Atividade de Programação Web II, para a implementação de tela de Cadastro e Login junto com Banco de Dados.</p>
     <p class="lead">
       <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Saber Mais</a>
     </p>
